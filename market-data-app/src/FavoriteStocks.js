b3da18fdf135a12f82934ce998c7d4417ce4a137
SeptemberFinesse <finessefilmproductions@gmail.com>
2024-06-19 20:41:57 -0400

    3rd try favorites

--- market-data-app/src/FavoriteStocks.js
+++ market-data-app/src/FavoriteStocks.js
@@ -21,36 +21,18 @@ const FavoriteStocks = () => {
   return (
     favorites.length > 0 && (
       <div className="favorite-stocks">
-        <h2>Favorite Stocks</h2>
-        <div className="table-container">
-          <table>
-            <thead>
-              <tr>
-                <th>Star</th>
-                <th>Symbol</th>
-                <th>Price</th>
-              </tr>
-            </thead>
-            <tbody>
-              {favorites.map(stock => {
-                const percentChange = ((stock.latestTrade.p - stock.dailyBar.o) / stock.dailyBar.o);
-                const lastPriceColor = getLastPriceColor(percentChange);
-                const starIcon = 'https://www.svgrepo.com/show/13695/star.svg';
-                return (
-                  <tr key={stock.symbol}>
-                    <td>
-                      <img src={starIcon} alt="Favorite" onClick={() => dispatch(removeFavorite(stock.symbol))} className="favorite-icon" />
-                    </td>
-                    <td>{stock.symbol}</td>
-                    <td style={{ color: lastPriceColor }}>
-                      {formatNumber(stock.latestTrade.p)}
-                    </td>
-                  </tr>
-                );
-              })}
-            </tbody>
-          </table>
-        </div>
+        {favorites.map(stock => {
+          const percentChange = ((stock.latestTrade.p - stock.dailyBar.o) / stock.dailyBar.o);
+          const lastPriceColor = getLastPriceColor(percentChange);
+          const starIcon = 'https://www.svgrepo.com/show/13695/star.svg';
+          return (
+            <div key={stock.symbol} className="favorite-stock-item">
+              <span>{stock.symbol}</span>
+              <img src={starIcon} alt="Favorite" onClick={() => dispatch(removeFavorite(stock.symbol))} className="favorite-icon" />
+              <span style={{ color: lastPriceColor }}>{formatNumber(stock.latestTrade.p)}</span>
+            </div>
+          );
+        })}
       </div>
     )
   );
